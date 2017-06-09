ef4b4f597f03d306e294eae5506b2dbe6a79918b
xiaofen.tan <1042841802@qq.com>
2017-06-09 19:18:01 +0800

    resultReport

--- yuxi/runTests.py
+++ yuxi/runTests.py
@@ -2,9 +2,13 @@
 
 import unittest
 import resultConf
-from testCases import init
+from testCases import yuxi_Chat
+from testCases import yuxi_Like
+from testCases import yuxi_Login
 
 testunit = unittest.TestSuite()
-testunit.addTest(unittest.makeSuite(init.Init))
+testunit.addTest((unittest.makeSuite(yuxi_Chat.YuxiChat)))
+testunit.addTest((unittest.makeSuite(yuxi_Like.YuxiLike)))
+testunit.addTest(unittest.makeSuite(yuxi_Login.YuxiLogin))
 
-resultConf.createReport(testunit, title=u"开源中国所有用例", desc=u"开源中国所有用例")
+resultConf.createReport(testunit, title=u"语戏所有用例", desc=u"语戏所有用例")
